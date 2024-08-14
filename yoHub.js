8130b18e3e7c9bb2014c98e549a06609781629b8
DaedraDante <153109749+DaedraDante@users.noreply.github.com>
2024-08-14 12:44:52 +0300

    actual v1.0
    this is the current code of the app , I got it from scratch , now its easier to read and understand and Im about to add local storage to it.

--- yoHub.js
+++ yoHub.js
@@ -1,55 +1,43 @@
-const btnSection1 = document.getElementById("btn-open-section1");
-const contentSection1 = document.getElementById("section1-content");
-const btn1ContentSection1 = document.getElementById("section1-content-button1");
-const sectionSelectScreen = document.getElementById("section-select");
-const section1AddTaskBtn = document.getElementById("section1-addTaskBtn");
-const activitiesTable = document.getElementById("activities-table");
+console.log("console is working");
+const sctSelectScreen = document.getElementById("sct-select-screen");
+const sct1 = document.getElementById("sct1");
+const sct1taskTable = document.getElementById("task-table")
+const sct1Open = document.getElementById("sct1-Open");
+const sct1AddTaskBtn = document.getElementById("sct1-AddTaskBtn")
+const sct1Close = document.getElementById("sct1-Close");
+const sct2Open = document.getElementById("sct2-Open");
+// loadToDoList();
 
+//Open and Close sct1
+sct1Open.addEventListener("click",() => {
+        sctSelectScreen.style.display = "none"
+        sct1.style.display = "block"
+});
+sct1Close.addEventListener("click",() => {
+        sct1.style.display = "none"
+        sctSelectScreen.style.display = "block"
+});
 
-contentSection1.style.display = "none"
-
-btnSection1.addEventListener("click",()=> {
-    
-        contentSection1.style.display = "block" 
-})
-btn1ContentSection1.addEventListener("click",() => {
-
-        contentSection1.style.display = "none"
-
-})
-
-
-section1AddTaskBtn.addEventListener("click",() => {
-
-  //Create input1
-  const newTableRowInput1 = document.createElement("input");
-  //Create input2
-  const newTableRowInput2 = document.createElement("input");
-  //Create section/options
-  const newTableRowSelect = document.createElement("select");
-  const newTableRowOption1 = document.createElement("option");
-  const newTableRowOption2 = document.createElement("option");
-  const newTableRowOption3 = document.createElement("option");
-
-  //Set properties for input1
-  newTableRowInput1.className = "alligned-table-inner";
-  //Set properties for input2
-  newTableRowInput2.className = "alligned-table-inner";
-  //Set properties for select/options
-  newTableRowSelect.className = "alligned-table-inner";
-  newTableRowOption1.value = "1";
-  newTableRowOption1.text = "1";
-  newTableRowOption2.value = "2";
-  newTableRowOption2.text = "2";
-  newTableRowOption3.value = "3";
-  newTableRowOption3.text = "3";
-  //Add options to select element using the .add method
-  newTableRowSelect.add(newTableRowOption1);
-  newTableRowSelect.add(newTableRowOption2);
-  newTableRowSelect.add(newTableRowOption3);
-
-  //Append input1 to activitiesTable
-  activitiesTable.appendChild(newTableRowInput1);
-  activitiesTable.appendChild(newTableRowInput2);
-  activitiesTable.appendChild(newTableRowSelect);
-})
+//Add task function and event listener
+const addTask = () => {
+        // alert("added a task")
+        const taskTemplateInput1 = document.createElement("input");
+        const taskTemplateInput2 = document.createElement("input");
+        const taskTemplateSelect = document.createElement("select");
+        const taskTemplateSelectOption1 = document.createElement("option");
+        const taskTemplateSelectOption2 = document.createElement("option");
+        const taskTemplateSelectOption3 = document.createElement("option");
+        taskTemplateSelectOption1.textContent = "1";
+        taskTemplateSelectOption2.textContent = "2";
+        taskTemplateSelectOption3.textContent = "3";
+        taskTemplateSelect.add(taskTemplateSelectOption1);
+        taskTemplateSelect.add(taskTemplateSelectOption2);
+        taskTemplateSelect.add(taskTemplateSelectOption3);
+        taskTemplateInput1.className = "task-table-child"
+        taskTemplateInput2.className = "task-table-child"
+        taskTemplateSelect.className = "task-table-child"
+        sct1taskTable.appendChild(taskTemplateInput1);
+        sct1taskTable.appendChild(taskTemplateInput2);
+        sct1taskTable.appendChild(taskTemplateSelect);
+};
+sct1AddTaskBtn.addEventListener("click",addTask);
